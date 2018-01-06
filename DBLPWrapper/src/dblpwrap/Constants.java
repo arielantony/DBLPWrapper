cdfa7e2b9f5d48752f1269a2e65f69371a2dbdb9
Ariel Afonso <aaa@icomp.ufam.edu.br>
2018-01-06 19:39:55 -0400

    Reading Inproceedings and Generating Insert File

--- DBLPWrapper/src/dblpwrap/Constants.java
+++ DBLPWrapper/src/dblpwrap/Constants.java
@@ -2,8 +2,7 @@ package dblpwrap;
 
 public class Constants {
 	public static final String REQUIRED_FILE_XML = "dblp.xml";
-	public static final String GENERATED_FILE_SQL = "insertSQLFileName";
-	public static final String OUTPUT_FILE_PATH = "";
+	public static final String OUTPUT_FILE_PATH = "SQLInserts/";
 	public static final int LOWER_YEAR_LIMIT = 2005;
 	public static final int HIGHER_YEAR_LIMIT = 2017;
 }
